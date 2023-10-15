5188bfa36a7d0268fd727494dad3d62cf5b444dd
Abhideep Maity <abhideep.maity@Abhideeps-MacBook-Pro.local>
2023-10-15 14:05:07 +0530

    exercise

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(numberOfApples: i32) -> i32 {
+    if numberOfApples <= 40 {
+        numberOfApples * 2
+    } else {
+        numberOfApples
+    }
+}
 
 // Don't modify this function!
 #[test]
